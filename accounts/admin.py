a7687c5e93f1c69125a3b040fa377d89af99550b
Ali Jaladati <jaladati1387@gmail.com>
2024-05-09 16:02:28 +0330

    Create user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "email_activation_code", "is_active", "is_staff", "is_superuser"]
+    list_editable = ["email", "email_activation_code", "is_active", "is_staff", "is_superuser"]
+    list_filter = ["is_active", "is_staff", "is_superuser"]
+    search_fields = ["username", "email", "phone", "address"]
